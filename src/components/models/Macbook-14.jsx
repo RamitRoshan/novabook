dded8e22f67386d0b47da7cf9c4b024a5c3af5d6
Ramit Roshann <ramitroshan777777@gmail.com>
2026-02-16 19:27:03 +0530

    Macbook-14, 16: implements color change in both macbook 14 and 16

--- src/components/models/Macbook-14.jsx
+++ src/components/models/Macbook-14.jsx
@@ -8,9 +8,11 @@ Source: https://sketchfab.com/3d-models/macbook-pro-m3-16-inch-2024-8e34fc2b3031
 Title: macbook pro M3 16 inch 2024
 */
 
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { useGLTF, useTexture } from '@react-three/drei'
 import useMacbookStore from '../../store';
+import { noChangeParts } from '../../constants';
+import {Color} from 'three';
 
 export default function MacbookModel14(props) {
 
@@ -23,6 +25,9 @@ export default function MacbookModel14(props) {
     scene.traverse((child) => {
       if(child.isMesh){
         //change color only if the part name is NOT noChangeParts
+        if(!noChangeParts.includes(child.name)) {
+          child.material.color = new Color(color);
+        }
       }
     })
   }, [color]);
